ff5dca0568cc7691588e4e1261b16fea128e2e51
Mohit Mehndiratta <mkm321.mm@gmail.com>
2018-09-27 21:09:18 +0530

    Aerospike assignment till Data Dumper tools

--- DataDumpTool/DataDumpTool/Program.cs
+++ DataDumpTool/DataDumpTool/Program.cs
@@ -21,28 +21,32 @@ namespace DataDumpTool
             string nameSpace = "AirEngine";
             string setName = "Mohit";
             int flag = 0;
+            int count=0;
             foreach (var rec in record) // Each record will be fetched and printed on the screen
             {
-                if (flag != 0)
+                if(count<=20000)
                 {
-                    var url = rec.url;
-                    string[] urlKey = url.Split('/');
-                    var key = new Key(nameSpace, setName, urlKey[5]);
-                    aerospikeClient.Put(new WritePolicy(), key, new Bin[] {
-                        new Bin("author", rec.author),
-                        new Bin("content",rec.content),
-                        new Bin("region",rec.region),
-                        new Bin("language",rec.language),
-                        new Bin("following",rec.following),
-                        new Bin("followers",rec.followers),
-                        new Bin("id",rec.id),
-                        new Bin("retweet",rec.retweet),
-                        new Bin("authorid",rec.authorid)
-                    });
-                }
-                else
-                {
-                    flag = 1;
+                    if (flag != 0)
+                    {
+                        var url = rec.url;
+                        string[] urlKey = url.Split('/');
+                        var key = new Key(nameSpace, setName, urlKey[5]);
+                        aerospikeClient.Put(new WritePolicy(), key, new Bin[] {
+                            new Bin("author", rec.author),
+                            new Bin("content",rec.content),
+                            new Bin("region",rec.region),
+                            new Bin("language",rec.language),
+                            new Bin("following",rec.following),
+                            new Bin("followers",rec.followers),
+                            new Bin("id",rec.id),
+                            new Bin("retweet",rec.retweet),
+                            new Bin("authorid",rec.authorid)
+                        });
+                    }
+                    else
+                    {
+                        flag = 1;
+                    }
                 }
             }
             sr.Close();
