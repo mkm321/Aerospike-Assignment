e1d9ffb9cbdc0c993ee26ad15f5696c706b6cad5
tavisca-vchauhan <vchauhan@tavisca.com>
2018-09-29 16:38:21 +0530

    Added aerospike web api assignment performing get, put,delete

---
+++ AerospikleWebApi/AerospikleWebApi/Models/Records.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AerospikleWebApi.Models
+{
+    public class Records
+    {
+        public string author { get; set; }
+        public string content { get; set; }
+        public string region { get; set; }
+        public string language { get; set; }
+        public int following { get; set; }
+        public int followers { get; set; }
+        public string url { get; set; }
+        public int retweet { get; set; }
+        public string id { get; set; }
+        public string authorid { get; set; }
+    }
+}
